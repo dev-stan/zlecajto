b92c70b2189520d6e89b89ddc98f57e32e167cb9
dev-stan <stachuzalewski9@gmail.com>
2025-08-22 10:44:12 +0200

    remove roles from project entirely

--- db/schema.rb
+++ db/schema.rb
@@ -51,13 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_22_120000) do
     t.index ["task_id"], name: "index_reviews_on_task_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_roles_on_name", unique: true
-  end
-
   create_table "submissions", force: :cascade do |t|
     t.string "status"
     t.bigint "task_id", null: false
@@ -87,15 +80,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_22_120000) do
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table "user_roles", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "role_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_user_roles_on_role_id"
-    t.index ["user_id"], name: "index_user_roles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -118,6 +102,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_22_120000) do
   add_foreign_key "submissions", "tasks"
   add_foreign_key "submissions", "users"
   add_foreign_key "tasks", "users"
-  add_foreign_key "user_roles", "roles"
-  add_foreign_key "user_roles", "users"
 end
