5b647f3250ef715bedfe77462281604255f44890
dev-stan <stachuzalewski9@gmail.com>
2025-08-29 07:03:35 +0200

    clean up home page syntax

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def home; end
+  def home
+    @accordion_elements = [['Opisz z czym potrzebujesz pomocy', '1'], ['Zdefiniuj cenę usługi', '2'],
+                           ['Otrzymaj oferty i wybierz tą która tobie odpowiada', '3']]
+  end
 end
