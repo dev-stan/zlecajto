251e090e53c199232af22aabb6dd4475a9428089
dev-stan <stachuzalewski9@gmail.com>
2025-09-13 17:22:51 +0200

    waitlist routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root 'pages#home'
+  root 'pages#waitlist'
   get 'profile', to: 'pages#profile'
   resources :tasks, only: %i[index create show new update] do
     collection do
