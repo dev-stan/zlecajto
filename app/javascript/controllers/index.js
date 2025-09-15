d19ee64f3cea728e4f906dbb21c16b299f49b834
dev-stan <stachuzalewski9@gmail.com>
2025-09-15 03:53:30 +0200

    js fix #4 prod

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import ImageUploadController from "controllers/image_upload_controller"
-application.register("image-upload", ImageUploadController)
 eagerLoadControllersFrom("controllers", application)
