7c19968a1bbb889556bfb874069624117435b844
dev-stan <stachuzalewski9@gmail.com>
2025-09-15 14:34:48 +0200

    clean up task card components

---
+++ app/components/tasks/new_task_card_component.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Tasks
+  #  - :default (title first, badge under)
+  #  - :owner   (badge first, title under)
+  class NewTaskCardComponent < ApplicationComponent
+    include ActionView::Helpers::DateHelper
+
+    renders_one  :image
+    renders_many :badges
+
+    attr_reader :task, :variant, :path
+
+    def initialize(task:, path: nil, variant: :default)
+      super()
+      @task = task
+      @path = path
+      @variant = variant.to_sym
+    end
+
+    private
+
+    # helper methods
+    def badges_before_title?
+      variant == :owner
+    end
+
+    # dynamic classes
+    def title_classes
+      base = 'text-xl font-semibold text-gray-900 mb-2'
+      badges_before_title? ? "#{base} mt-4" : base
+    end
+  end
+end
