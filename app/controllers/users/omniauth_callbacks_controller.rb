74a6d896dc07ebd3da83cb3cf51698f25fdd5118
dev-stan <stachuzalewski9@gmail.com>
2025-09-22 01:02:42 +0200

    add google omniauth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,22 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    @user = User.from_google(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in @user, event: :authentication
+      if @user.phone_number.blank?
+        redirect_to edit_users_profile_path, notice: 'Uzupełnij numer telefonu, aby dokończyć rejestrację.'
+      else
+        redirect_to root_path, notice: 'Zalogowano przez Google!'
+      end
+      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
