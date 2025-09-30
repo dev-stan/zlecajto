7b09dc017a5ab3231fec67794bf01794059427f1
dev-stan <stachuzalewski9@gmail.com>
2025-09-30 16:11:40 +0200

    fix manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,5 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+// Link the PWA web app manifest (file lives at app/assets/manifest.json.erb)
+//= link manifest.json
