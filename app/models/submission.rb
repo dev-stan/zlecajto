fed5ff82cb2d1636cbd2da0cbe0d01e5301193fe
dev-stan <stachuzalewski9@gmail.com>
2025-10-02 01:42:58 +0200

    fix bug

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -37,18 +37,11 @@ class Submission < ApplicationRecord
 
     errors.add(:base, 'Nie możesz zgłosić się do własnego zadania')
   end
-  # Variables i will need for send accepted submission email
-  # {
-  # "due_date": "test_due_date",
-  # "location": "test_location",
-  # "salary": "test_salary",
-  # "timeslot": "test_timeslot",
-  # "title": "test_title"
-  # }
 
-  send_accepted_submission_email
-  MailgunTemplateJob.perform_later(to: user.email, template: 'prod_submission_accepted', subject: 'Twoje zgłoszenie zostało zaakceptowane!',
-                                   variables: { due_date: task.due_date.strftime('%d/%m/%Y'), location: task.location, salary: task.salary, timeslot: task.timeslot, title: task.title, task_url: task_url(task.id) })
+  def send_accepted_submission_email
+    MailgunTemplateJob.perform_later(to: user.email, template: 'prod_submission_accepted', subject: 'Twoje zgłoszenie zostało zaakceptowane!',
+                                     variables: { due_date: task.due_date.strftime('%d/%m/%Y'), location: task.location, salary: task.salary, timeslot: task.timeslot, title: task.title, task_url: task_url(task.id) })
+  end
 
   def send_new_submission_email
     MailgunTemplateJob.perform_later(to: task.user.email, template: 'prod_new_submission', subject: 'Nowe zgłoszenie!',
