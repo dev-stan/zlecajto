8eda832968f95633e5440cfe190d8910e7b7e406
dev-stan <stachuzalewski9@gmail.com>
2025-10-04 11:52:30 +0200

    add logout for desktop

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -41,7 +41,7 @@ class Submission < ApplicationRecord
 
   def send_accepted_submission_email
     MailgunTemplateJob.perform_later(to: user.email, template: 'prod_submission_accepted', subject: 'Twoje zgłoszenie zostało zaakceptowane!',
-                                     variables: { due_date: task.due_date.strftime('%d/%m/%Y'), location: task.location, salary: task.salary, timeslot: task.timeslot, title: task.title, task_url: task_url(task.id) })
+                                     variables: { due_date: task.due_date.strftime('%d/%m/%Y'), location: task.location, salary: task.salary, timeslot: task.timeslot, title: task.title, task_url: task_url(task.id), task_url: task_url(task.id) })
   end
 
   def send_new_submission_email
