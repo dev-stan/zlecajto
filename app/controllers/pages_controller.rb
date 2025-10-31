fa8d74c80e3693f829763f458e97c41eeffa96d9
dev-stan <stachuzalewski9@gmail.com>
2025-10-31 14:11:56 +0100

    add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@
 
 class PagesController < ApplicationController
   def home
-    console
     @accordion_elements = [['Opisz z czym potrzebujesz pomocy', '1'], ['Zdefiniuj cenę usługi', '2'],
                            ['Otrzymaj oferty i wybierz tą która tobie odpowiada', '3']]
 
