6f5131f1bc476d36907aca583674d6352838813a
dev-stan <stachuzalewski9@gmail.com>
2025-11-20 22:28:31 +0100

    fix task status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_27_163957) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_20_212550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -129,7 +129,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_27_163957) do
     t.string "title"
     t.text "description"
     t.integer "salary"
-    t.string "status", default: "Otwarte"
+    t.string "status", default: "open"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
