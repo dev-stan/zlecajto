8d0842661dc3996b04bf9442f3f433acb9368d41
dev-stan <stachuzalewski9@gmail.com>
2025-11-24 16:11:48 +0100

    more specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :user do
-    first_name { 'Marta' }
-    last_name { 'Skubis' }
-    phone_number { '334234123' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone_number { Faker::PhoneNumber.cell_phone_in_e164.delete_prefix('+') }
     email { Faker::Internet.email }
     password { 'password123' }
   end
