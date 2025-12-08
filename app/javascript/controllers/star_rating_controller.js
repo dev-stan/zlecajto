179e8683be2659bf3fe55acd7064b5c582e37a79
dev-stan <stachuzalewski9@gmail.com>
2025-12-08 12:24:36 +0100

    remove redundant code

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -8,43 +8,26 @@ export default class extends Controller {
   }
 
   rate(event) {
-    const rating = event.currentTarget.dataset.value
-    this.inputTarget.value = rating
+    this.inputTarget.value = event.currentTarget.dataset.value
     this.updateStars()
   }
 
   updateStars() {
     const currentRating = parseInt(this.inputTarget.value) || 0
-    this.starTargets.forEach((star, index) => {
-      const starValue = index + 1
-      const icon = star.querySelector("i")
-      
-      // Ensure transition classes are present
-      if (!icon.classList.contains("transition-all")) {
-        icon.classList.add("transition-all", "duration-200", "ease-in-out")
-      }
-      
-      const isSelected = starValue <= currentRating
-      const isSolid = icon.classList.contains("fa-solid")
 
-      if (isSelected && !isSolid) {
-        // Becoming selected
+    this.starTargets.forEach((star, index) => {
+      const icon = star.querySelector("i")
+      const isSelected = (index + 1) <= currentRating
+      const isPrimary = icon.classList.contains("text-primary")
+
+      // transitions
+      icon.classList.add("fas", "transition-all", "duration-200", "ease-in-out")
+
+      if (isSelected !== isPrimary) {
         this.animateChange(icon, () => {
-          icon.classList.remove("fa-regular", "text-gray-300")
-          icon.classList.add("fa-solid", "text-primary")
+          icon.classList.toggle("text-primary", isSelected)
+          icon.classList.toggle("text-gray-300", !isSelected)
         })
-      } else if (!isSelected && isSolid) {
-        // Becoming unselected
-        this.animateChange(icon, () => {
-          icon.classList.remove("fa-solid", "text-primary")
-          icon.classList.add("fa-regular", "text-gray-300")
-        })
-      } else if (isSelected && isSolid) {
-        // Ensure correct color if already selected (e.g. fixing previous gray-400)
-        if (icon.classList.contains("text-gray-400")) {
-           icon.classList.remove("text-gray-400")
-           icon.classList.add("text-primary")
-        }
       }
     })
   }
